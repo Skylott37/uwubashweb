17521af6e24b148a7b5005776249ab1b8daf176c
Skylott37 <136955141+Skylott37@users.noreply.github.com>
2023-12-24 18:57:00 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,7 @@
+wget -c https://skylott37.github.io/uwubashweb/uwuBash.tar
+tar -xf uwuBash.tar -C /usr/bin
+rm uwuBash.tar
+echo -e  "\033[1;35muwuBash was been installed!"
+echo  -e "\033[1;35mto get version write: uwu -u"
+echo  -e "\033[1;35mUwU"
+#rm installer.sh
